c4cc56f127c88fd638df1a7f31f4f170cbf3e626
judy-bw <jbw5@williams.edu>
2026-03-12 08:38:00 -0400

    Update 2026-03-11-RGB-loop.c

--- _posts/2026-03-11-RGB-loop.c
+++ _posts/2026-03-11-RGB-loop.c
@@ -13,6 +13,6 @@ author: Judy
 For this assignment, we were practicing while statements. So, we had to make a loop that would print out RED three times, then GREEN three times, then BLUE three times, only on the 10th run through of the loop. 
 Here's a photo of the output if you input 60 degrees. 
 
-![Embroidery](/assets/img/Screenshot.png)
+![Embroidery](/assets/img/Screenshot4.PNG)
 
 A tip I would give myself is to make sure to count from zero!
