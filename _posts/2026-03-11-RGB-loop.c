90c555f601dd4ca691da4ff685887b3749e9c622
judy-bw <jbw5@williams.edu>
2026-03-12 08:36:33 -0400

    Update 2026-03-11-RGB-loop.c

--- _posts/2026-03-11-RGB-loop.c
+++ _posts/2026-03-11-RGB-loop.c
@@ -1,1 +1,18 @@
+---
+layout: post
+title: RGB Loop
+gh-repo: daattali/beautiful-jekyll
+comments: true
+mathjax: true
+author: Judy
+---
 
+{: .box-success}
+## This is a post to show you my RGB Loop assignment!
+
+For this assignment, we were practicing while statements. So, we had to make a loop that would print out RED three times, then GREEN three times, then BLUE three times, only on the 10th run through of the loop. 
+Here's a photo of the output if you input 60 degrees. 
+
+![Embroidery](/assets/img/Screenshot.png)
+
+A tip I would give myself is to make sure to count from zero!
